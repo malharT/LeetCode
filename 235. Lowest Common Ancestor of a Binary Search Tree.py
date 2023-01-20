19921629f3c986adc97a102cfcf7afc00bd01a10
Malhar Manohar Thombare <malharthombare@gmail.com>
2023-01-20 12:00:30 -0800

    Leetcode Problem 235 optimized

--- 235. Lowest Common Ancestor of a Binary Search Tree.py
+++ 235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -6,13 +6,16 @@
 #         self.right = None
 
 class Solution:
+    def LCA(self, root, p, q):
+        if p.val > root.val:
+            return self.lowestCommonAncestor(root.right, p ,q)
+        elif q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p ,q)
+        elif (p.val < root.val and q.val > root.val) or p.val==root.val or q.val==root.val:
+            return root
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        node = root
-        while True:
-            if node.val > p.val and node.val > q.val:
-                node = node.left
-            elif node.val < p.val and node.val < q.val:
-                node = node.right
-            else:
-                break
-        return node
+        if q.val < p.val:
+            temp = p
+            p = q
+            q = temp
+        return self.LCA(root, p, q)

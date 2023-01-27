0481982b67a19deec60b3ddc4bc9d7085d5d4952
Malhar Manohar Thombare <malharthombare@gmail.com>
2023-01-27 12:00:32 -0800

    Leetcode Problem 98 optimized

--- 98. Validate Binary Search Tree.py
+++ 98. Validate Binary Search Tree.py
@@ -5,22 +5,14 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isValidBST2(self, root: Optional[TreeNode], min_val, max_val) -> bool:
-        valid = True
-        if root.left is not None:
-            if root.left.val < root.val and root.left.val > min_val:
-                valid &= self.isValidBST2(root.left, min_val, root.val)
-            else:
-                return False
-        if not valid:
+    def traverse(self, root, min_val, max_val):
+        if root is None:
+            return True
+        if root.val > min_val and root.val < max_val:
+            return self.traverse(root.left, min_val, root.val) and self.traverse(root.right, root.val, max_val)
+        else:
             return False
-        if root.right is not None:
-            if root.right.val > root.val and root.right.val < max_val:
-                valid &= self.isValidBST2(root.right, root.val, max_val)
-            else:
-                return False
-        return valid
 
 
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        return self.isValidBST2(root, float('-inf'), float('inf'))
+        return self.traverse(root, float('-inf'), float('inf'))
